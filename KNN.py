3cf5f2c1a1bdad86cab57bad0a4c7f0931a77554
jakob-98 <jakobserlier@gmail.com>
2021-02-02 10:30:39 +0100

    setup for KNN

--- KNN.py
+++ KNN.py
@@ -1,9 +1,20 @@
 # %%
 import math
-import numpy as np
 
 
 # %% 
-def knn(k):
+def knn(x, k):
+    neighbours = {}
+    for i in range(k):
+        # closests = #min of distance
+        closest = None
+        neighbours[closest] = neighbours.get(closest, 0) + 1
+
+    return max(neighbours, key=neighbours.get)
+
+
+
+def distance(a, b):
+    # TODO implement eucledian distance
     pass
 
